c15d84bcbeb5136babe09d66179b53f7660457cf
RafaZeero <rafaa99@gmail.com>
2024-02-18 15:14:51 -0300

    testing redis

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func main() {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	ctx := context.Background()
+
+	// err := client.Set(ctx, "name", "Rafael", 0).Err()
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// val, err := client.Get(ctx, "name").Result()
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// fmt.Println("name", val)
+
+	u := map[string]string{"name": "Rafael", "age": "30", "class": "bard"}
+
+	for k, v := range u {
+		err := client.HSet(ctx, "user", k, v).Err()
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	data := client.HGetAll(ctx, "user").Val()
+
+	fmt.Println(data)
+}
